4e44a4ae9c5c4ff3eee1bc6d11aac567682f7765
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-16 01:52:04 +0200

    add all info on recipe page

--- src/RecipePage.jsx
+++ src/RecipePage.jsx
@@ -1,22 +1,77 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 import classes from "./RecipePage.module.css";
+import NotFound from "./NotFound";
 
 const RecipePage = () => {
+  const [recipeInfo, setRecipeInfo] = useState([]);
+  const [error, setError] = useState(false);
+  const [flag, setFlag] = useState("");
   const { recipeId } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(recipeId);
-  }, [recipeId]);
+    axios
+      .get(`http://localhost:3004/recipes`)
+      .then((data) => {
+        const allRecipes = data.data;
+        const found = allRecipes.find((recipe) => recipe.id == recipeId);
+        if (found === undefined) setError(true);
+        setRecipeInfo(found);
+      })
+      .then(
+        axios
+          .get(`https://restcountries.com/v3.1/name/${recipeInfo.country}`)
+          .then((data) => {
+            setFlag(data.data[0].flags.svg);
+          })
+          .catch((err) => {
+            setFlag("");
+          })
+      );
+  }, []);
 
-  return (
-    <div>
-      <h1>Recipe page {recipeId}</h1>
-      <button onClick={() => navigate(-1)}>Go back</button>
-    </div>
-  );
+  if (error) return <NotFound />;
+  else
+    return (
+      <div>
+        <button onClick={() => navigate(-1)}>{`<-`}</button>
+        <h1>{recipeInfo.name}</h1>
+        <div className={classes.flag_container}>
+          <img className={classes.flag} src={flag} alt={recipeInfo.country} />
+        </div>
+        <p>by {recipeInfo.author || "unknown author"}</p>
+        <div className={classes.image_container}>
+          <img
+            className={classes.image}
+            src={recipeInfo.image}
+            alt={recipeInfo.name}
+          />
+        </div>
+        <div className={classes.ingredient_list}>
+          <table>
+            <thead>
+              <tr>
+                <th>Ingredient</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {recipeInfo.ingredients &&
+                recipeInfo.ingredients.map((ingredient) => (
+                  <tr>
+                    <td>{ingredient.ingredient}</td>
+                    <td>{ingredient.quantity}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+        <p>{recipeInfo.instructions}</p>
+      </div>
+    );
 };
 
 export default RecipePage;
